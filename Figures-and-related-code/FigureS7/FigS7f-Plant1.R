87fc8aefe876d6db91ea89fcab37bcec3872142c
MoriMori <143572464+ZhangGeyu@users.noreply.github.com>
2026-01-27 15:28:18 +0800

    Create FigS7f-Plant1.R

---
+++ Figures-and-related-code/FigureS7/FigS7f-Plant1.R
@@ -0,0 +1,34 @@
+Figure_Theme <- theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank())+
+  theme(plot.title=element_text(size=8))+
+  theme(axis.text.x = element_text(angle=0, hjust=0.5))+
+  theme(axis.text.x = element_text(colour="black", size=8))+
+  theme(axis.text.y = element_text(colour="black", size=8))+
+  theme(axis.title=element_text(size=8))+
+  theme(axis.ticks=element_line(colour="black",size=0.5))+
+  theme(axis.line=element_line(colour="black")) + 
+  theme(legend.title=element_text(size=8))+
+  theme(legend.text=element_text(size=8))+
+  theme(axis.line=element_blank()) + 
+  theme(panel.border = element_rect(fill=NA, size=1))
+
+# Plant1
+
+Parental_SNP <- read.table('Parental_CallSNP_Plant1.txt', header = T)
+head(Parental_SNP)
+
+UMI_mut_counts <- table(Parental_SNP$SampleName_UMI)
+UMI_mut_counts <- as.data.frame(UMI_mut_counts)
+names(UMI_mut_counts) <- c("SampleName_UMI", "count")
+head(UMI_mut_counts)
+
+Mut_count_distribution <- ggplot(UMI_mut_counts, aes(x = count)) +
+  geom_histogram(aes(y = after_stat(density)),
+                 bins = 30,fill = "lightblue", colour = 'black', alpha = 0.5) +
+  labs(x = "Mutation Count per Readout",y = "Density",
+       title = "Plant1: mutation num in readouts") +
+  scale_x_continuous(breaks = seq(0, 200, by = 20)) + 
+  scale_y_continuous(breaks = seq(0, 5, by = 0.01)) + 
+  Figure_Theme
